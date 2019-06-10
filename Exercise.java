273886531b95a8123e1d6cc7b7541ff71a99ea40
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-10 14:17:45 +0530

    Create Exercise.java

---
+++ Exercise.java
@@ -0,0 +1,13 @@
+package Example;
+import java.util.*;
+public class Exercise{
+	public static void main(String []args){
+		Scanner scan=new Scanner(System.in);
+		String s=scan.next();
+		StringBuffer str=new StringBuffer(s);
+		System.out.println(str.reverse());
+		
+		
+	}
+
+}
