08195623666d9599014a2411fbe924705035f6e7
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-10 17:47:08 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package Example;
+import java.util.*;
+public class Hello {
+	public static void main(String[] args){
+	Scanner scan=new Scanner(System.in);
+		int N=scan.nextInt();
+		for(int i=0;i<N;i++){
+			System.out.println("Hello");
+		}
+	}
+}
