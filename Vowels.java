4024561f643e5bef85ec3ca49f709c4521dfaf3a
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-10 17:12:42 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,19 @@
+package Example;
+import java.util.*;
+public class Vowels {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		char ch=scan.next().charAt(0);
+		if( ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+			System.out.println("vowels");
+			
+		}
+		else if((ch>='a'||ch<='z')){
+			System.out.println("consonants");
+		}
+		else{
+			System.out.println("invalid");
+		}
+	}
+
+}
