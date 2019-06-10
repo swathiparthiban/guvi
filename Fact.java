65914cbaeed0a5a54eb1dee4c880bd1f91d79abe
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-10 14:31:18 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,14 @@
+package Example;
+import java.util.*;
+public class Fact {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++){
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+
+}
