f017d72dee40be26384b8e78278353a55ab2657a
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-11 15:10:30 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,25 @@
+package Example;
+import java.util.*;
+public class Large {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int N=scan.nextInt();
+		int a[]=new int[N];
+		for(int i=0;i<N;i++){
+			a[i]=scan.nextInt();
+		}
+		for(int i=0;i<N;i++){
+			for(int j=i+1;j<N;j++){
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+				
+			}
+			
+			}
+		for(int i=0;i<N;i++){
+			System.out.println(a[i]);
+		}
+	}
+
+}
