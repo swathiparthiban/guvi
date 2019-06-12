258c46ef8285c7b5cee7fcbd4883c4e4d08a72c7
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-12 15:24:56 +0530

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,13 @@
+package Example;
+import java.util.*;
+public class CamelCase {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		for(String str:s.split(" ")){
+			String t=str.substring(0,1);
+			System.out.print(t.toUpperCase()+str.substring(1)+" ");
+		}
+	}
+
+}
