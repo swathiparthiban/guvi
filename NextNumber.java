544377a5ee73ada837e61e70c42f737aa9a6399b
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-13 11:37:34 +0530

    Create NextNumber.java

---
+++ NextNumber.java
@@ -0,0 +1,11 @@
+package Example;
+import java.util.*;
+public class NextNumber {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int sum=n+1;
+		System.out.println(sum);
+	}
+
+}
