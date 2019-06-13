10e55c2aa1623158dd56fe80594217fc235dea7c
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-13 11:21:41 +0530

    Create SumOfTwo.java

---
+++ SumOfTwo.java
@@ -0,0 +1,12 @@
+package Example;
+import java.util.*;
+public class SumOfTwo {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
