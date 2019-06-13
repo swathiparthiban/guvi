8988cff81b992d293269417ffdec82a39f0e14ec
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-13 15:35:51 +0530

    Create Withoutcmp.java

---
+++ Withoutcmp.java
@@ -0,0 +1,16 @@
+package Example;
+import java.util.*;
+public class Withoutcmp {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		String str=scan.nextLine();
+		if(s.equalsIgnoreCase(str)){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+	}
+
+}
