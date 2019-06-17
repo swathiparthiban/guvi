5c9fef26542d2db2a3e347c3da45ae68601932c0
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-17 17:51:40 +0530

    Create AreaOfTriangle.java

---
+++ AreaOfTriangle.java
@@ -0,0 +1,12 @@
+package Example;
+import java.util.*;
+public class AreaOfTriangle {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int b=scan.nextInt();
+		int h=scan.nextInt();
+		int a=h*b/2;
+		System.out.println(a);
+	}
+
+}
