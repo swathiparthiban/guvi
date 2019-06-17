b4bc06cce0f001e343cbdc7557641b5571d86a4e
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-17 14:47:51 +0530

    Create DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,10 @@
+package Example;
+import java.util.*;
+public class DecimalToBinary {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		System.out.println(Integer.toBinaryString(n));
+	}
+
+}
