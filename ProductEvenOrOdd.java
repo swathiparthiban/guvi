99065d43f84e65343d8729c633ebb99e15173815
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-19 18:04:49 +0530

    Create ProductEvenOrOdd.java

---
+++ ProductEvenOrOdd.java
@@ -0,0 +1,16 @@
+package Example;
+import java.util.*;
+public class ProductEvenOrOdd {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		int n1=scan.nextInt();
+		int n2=scan.nextInt();
+		if((n1*n2)%2==0){
+			System.out.println("even");
+		}
+		else{
+		System.out.println("odd");
+	}
+
+}
+}
