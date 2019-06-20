aed01cad57cd6dc262c560c95dd77e68cc8a3930
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-20 12:24:28 +0530

    Create MinimumElements.java

---
+++ MinimumElements.java
@@ -0,0 +1,15 @@
+package Example;
+import java.util.*;
+public class MaximumElements {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=scan.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.println(a[0]);
+	}
+
+}
