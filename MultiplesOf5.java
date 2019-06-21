6b897004ad1730073d1c607faad358acf317e262
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-21 14:02:13 +0530

    Create MultiplesOf5.java

---
+++ MultiplesOf5.java
@@ -0,0 +1,10 @@
+package Example;
+import java.util.*;
+public class MultiplesOf5 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		System.out.println(n*1+" "+n*2+" "+n*3+" "+n*4+" "+n*5);
+	}
+
+}
