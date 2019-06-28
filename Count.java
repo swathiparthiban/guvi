b2f508215639ed2001b38f5cbc0e7eb2ed425bf3
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-06-28 13:33:20 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,10 @@
+package Example;
+import java.util.*;
+public class Count {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	int n=scan.nextInt();
+	String s=String.valueOf(n);
+	System.out.println(s.length());
+}
+}
