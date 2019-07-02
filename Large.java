792348725a46ee6161d053a03af3eee5f418a4d8
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-02 11:41:30 +0530

    Update Large.java

--- Large.java
+++ Large.java
@@ -10,15 +10,16 @@ public class Large {
 		}
 		for(int i=0;i<N;i++){
 			for(int j=i+1;j<N;j++){
+				if(a[i]<a[j]){
 				int temp=a[i];
 				a[i]=a[j];
 				a[j]=temp;
-				
+				}
 			}
 			
 			}
 		for(int i=0;i<N;i++){
-			System.out.println(a[i]);
+			System.out.print(a[i]);
 		}
 	}
 
