40fef238c65ab647f93305fdea62d861179ee0dd
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-03 16:14:44 +0530

    Create Evenintervals.java

---
+++ Evenintervals.java
@@ -0,0 +1,16 @@
+package Example;
+import java.util.*;
+public class bedSet214 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+		
+for(int i=n;i<n1;i++){			
+	if(i%2==0 && i!=n){
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
