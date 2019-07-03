72ba65e9578c8bcca83e74aa3359d9d41c33f2cb
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-03 18:54:07 +0530

    Create BegginerSet761.java

---
+++ BegginerSet761.java
@@ -0,0 +1,13 @@
+package Example;
+import java.util.*;
+public class BegginerSet761 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String s=scan.next();
+		int n=scan.nextInt();
+			System.out.print(s.substring(0,n));
+
+		}
+	}
+
+
