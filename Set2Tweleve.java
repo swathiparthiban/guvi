e695a62c3569e7e62e7d1a9f40674ef8b10ba929
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-04 10:37:02 +0530

    Create Set2Tweleve.java

---
+++ Set2Tweleve.java
@@ -0,0 +1,20 @@
+package Guvi;
+import java.util.*;
+public class Set2Tweleve {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int n1=scan.nextInt();
+		int []a=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=scan.nextInt();
+			}
+			Arrays.sort(a);
+			int b=0;
+			b=n-n1;
+			System.out.println(a[b]);
+			}		}
+	
+	
+
+
