407d4ecb1768530ab6105f7ae0cad86ab3452fbc
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-05 15:36:57 +0530

    Create WithoutSemicolon.java

---
+++ WithoutSemicolon.java
@@ -0,0 +1,12 @@
+package Guvi;
+import java.util.*;
+public class WithoutSemicolon {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		for(int i=1;i<2;System.out.println(s)){
+			i++;
+		}
+	}
+
+}
