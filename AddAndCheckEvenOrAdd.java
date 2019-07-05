caae5e27c7ed54dc686bdadd98ea03b41df74839
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-05 15:17:09 +0530

    Create AddAndCheckEvenOrAdd.java

---
+++ AddAndCheckEvenOrAdd.java
@@ -0,0 +1,17 @@
+package Guvi;
+import java.util.*;
+public class AddAndCheckEvenOrAdd {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int n2=scan.nextInt();
+		int a=n+n2;
+		if(a%2==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+		}
+	}
+
+}
