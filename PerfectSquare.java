361e36577121f9778cf8d734c11a37c681c78598
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-05 17:48:42 +0530

    Create PerfectSquare.java

---
+++ PerfectSquare.java
@@ -0,0 +1,19 @@
+package Guvi;
+import java.util.*;
+public class PerfectSquare {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		if(a==b){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+		
+
+		
+	}
+
+}
