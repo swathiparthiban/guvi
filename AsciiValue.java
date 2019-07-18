a84c2a0614cce8838fc52651110f918860b1a263
swathiparthiban <42937994+swathiparthiban@users.noreply.github.com>
2019-07-18 14:00:56 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,11 @@
+package Example;
+import java.util.*;
+public class AsciiValue {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		char ch=scan.next().charAt(0);
+		int n=(int)ch;
+        System.out.println(n);
+	}
+
+}
